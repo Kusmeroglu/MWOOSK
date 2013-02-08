83425421fd136b34b50d4cc3b7a1a920ff92a295
linden <linden.wright@gmail.com>
2013-02-08 00:29:11 -0500

    Fixing max armor and clear all

--- js/mwoosk.js
+++ js/mwoosk.js
@@ -498,8 +498,8 @@ $(function () {
 
     $('#clearArmor').on('click', function(e){
         limbList.forEach(function(limbName){
-            $('#'+limbName+' .armorspinner.front').attr('value', 0);
-            $('#'+limbName+' .armorspinner.rear').attr('value', 0);
+            $('#'+limbName+' .armorspinner.front').spinner("value", "0");
+            $('#'+limbName+' .armorspinner.rear').spinner("value", "0");
         });
     });
     $('#maxArmor').on('click', function(e){
@@ -507,11 +507,11 @@ $(function () {
             var maxarmor = mechObj.limbs[limbName].maxArmor;
             if ( $('#'+limbName+' .armorspinner.rear').length ){
                 var armor = Math.round(maxarmor * .6);
-                $('#'+limbName+' .armorspinner.rear').attr('value', maxarmor - armor);
+                $('#'+limbName+' .armorspinner.rear').spinner('value', maxarmor - armor);
             } else {
                 var armor = maxarmor;
             }
-            $('#'+limbName+' .armorspinner.front').attr('value', armor);
+            $('#'+limbName+' .armorspinner.front').spinner('value', armor);
         });
     });
 
